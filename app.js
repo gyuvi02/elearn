b64e6e448aaaf8310a0fbcdd488d19d8ba8f7ad0
gyuvi02 <gyulaother@gmail.com>
2020-09-13 10:14:25 +0200

    User routes added

--- app.js
+++ app.js
@@ -1,41 +1,158 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//add a date to every request
+app.use((req, res, next) => {
+  req.requestTime = new Date();
+  next();
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const getAllEbooks = (req, res) => {
+  res
+      .status(200)
+      .json({message: `Request date for all ebooks is ${req.requestTime.toLocaleDateString()}, ${req.requestTime.toLocaleTimeString()}`})
+};
+
+const getEbook = (req, res) => {
+  res
+      .status(200)
+      .json({
+        status: "success",
+        data: req.params.id
+      })
+};
+
+const createEbook = (req, res) => {
+  res.status(201);
+  console.log(req.body);
+  res.send(req.body)
+};
+
+const updateEbook = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: `The data in ebook ${req.params.id} was modified`
+  })
+};
+
+const deleteEbook = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+  })
+};
+
+const getAllUsers = (req, res) => {
+  res
+      .status(200)
+      .json({message: `Request date for all users is ${req.requestTime.toLocaleDateString()}, ${req.requestTime.toLocaleTimeString()}`})
+};
+
+const getUser = (req, res) => {
+  res
+      .status(200)
+      .json({message: `Request date for all users is ${req.requestTime.toLocaleDateString()}, ${req.requestTime.toLocaleTimeString()}`})
+};
+
+const createUser = (req, res) => {
+  res
+      .status(200)
+      .json({message: `Request date for all users is ${req.requestTime.toLocaleDateString()}, ${req.requestTime.toLocaleTimeString()}`})
+};
+
+const updateUser = (req, res) => {
+  res
+      .status(200)
+      .json({message: `Request date for all users is ${req.requestTime.toLocaleDateString()}, ${req.requestTime.toLocaleTimeString()}`})
+};
+
+const deleteUser = (req, res) => {
+  res
+      .status(200)
+      .json({message: `Request date for all users is ${req.requestTime.toLocaleDateString()}, ${req.requestTime.toLocaleTimeString()}`})
+};
+
+const ebookRouter = express.Router();
+app.use('/api/v1/ebooks', ebookRouter);
+
+ebookRouter.route('/')
+    .get(getAllEbooks)
+    .post(createEbook);
+
+ebookRouter.route('/:id')
+    .get(getEbook)
+    .patch(updateEbook)
+    .delete(deleteEbook);
+
+const userRouter = express.Router();
+app.use('/api/v1/users', userRouter);
+
+userRouter.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+userRouter.route('/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+
+const port = 3000;
+app.listen(port, ()=> {
+  console.log('App running on port 3000');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+//
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send('Hello from the server side');
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
